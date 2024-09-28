7057348bf819322fba252c9d10072cdd347ffdd2
18wh1a1260 <18wh1a1260@bvrithyderabad.edu.in>
2024-09-27 23:24:17 -0400

    Performed all 5 changes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import './App.css';
+import './App.scss';
+import './index.scss';
 
 import {
   BrowserRouter as Router,
@@ -13,6 +14,7 @@ import HomePage from './HomePage/HomePage';
 import Footer from './Footer/Footer';
 import AboutPage from './AboutPage/AboutPage';
 import LoginPage from './LoginPage/LoginPage';
+import BudgetChart from './BudgetChart/BudgetChart';
 
 
 function App() {
@@ -25,6 +27,7 @@ function App() {
         <Route path="/about" element={<AboutPage />} />
           <Route path="/login" element={<LoginPage />} />
           <Route path="/" element={<HomePage />} />
+          <Route path="/budget" element={<BudgetChart />} />
 
         </Routes>
       </div>
